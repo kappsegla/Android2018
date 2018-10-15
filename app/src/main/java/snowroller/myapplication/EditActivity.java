1496f5b3e347dd6c169a42a8ff35badb6f00f645
Martin Blomberg <martin.blomberg@outlook.com>
2018-10-15 15:34:20 +0200

    Result

--- app/src/main/java/snowroller/myapplication/EditActivity.java
+++ app/src/main/java/snowroller/myapplication/EditActivity.java
@@ -1,13 +1,30 @@
 package snowroller.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        editText = findViewById(R.id.editText2);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("TEXT");
+        editText.setText(text);
+    }
+
+    public void doneButtonClicked(View view) {
+        Intent answer = new Intent();
+        answer.putExtra("ANSWER", editText.getText().toString());
+
+        setResult(0, answer);
+        finish();
     }
 }
--- app/src/main/java/snowroller/myapplication/TestActivity.java
+++ app/src/main/java/snowroller/myapplication/TestActivity.java
@@ -1,13 +1,39 @@
 package snowroller.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
+
+    private final int ID = 1234;
+    private TextView v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        v = findViewById(R.id.textView3);
+    }
+
+    public void editButtonClicked(View view) {
+        String text = v.getText().toString();
+        Intent i = new Intent(this, EditActivity.class);
+        i.putExtra("TEXT", text);
+
+        startActivityForResult(i, ID);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if( requestCode == ID)
+        {
+            String answer = data.getStringExtra("ANSWER");
+            v.setText(answer);
+        }
     }
 }

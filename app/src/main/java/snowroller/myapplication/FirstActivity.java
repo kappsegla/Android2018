dbe30b3cd0b576f5fa0e2a7fa18b99d4395c1896
Martin Blomberg <martin.blomberg@outlook.com>
2018-10-15 14:59:27 +0200

    startActivity

--- app/src/main/java/snowroller/myapplication/FirstActivity.java
+++ app/src/main/java/snowroller/myapplication/FirstActivity.java
@@ -1,13 +1,28 @@
 package snowroller.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        editText = findViewById(R.id.editText);
+    }
+
+    public void sendButtonClicked(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        String text = editText.getText().toString();
+        intent.putExtra("MESSAGE", text);
+
+        startActivity(intent);
     }
 }

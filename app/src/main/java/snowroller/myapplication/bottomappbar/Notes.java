5186021cf78d3b9a77d3e3b05f31eae09d2e408c
Martin Blomberg <martin.blomberg@outlook.com>
2018-10-30 14:53:10 +0100

    SwipeToDelete

---
+++ app/src/main/java/snowroller/myapplication/bottomappbar/Notes.java
@@ -0,0 +1,15 @@
+package snowroller.myapplication.bottomappbar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import snowroller.myapplication.R;
+
+public class Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
--- app/src/main/java/snowroller/myapplication/list/ListActivity.java
+++ app/src/main/java/snowroller/myapplication/list/ListActivity.java
@@ -1,8 +1,10 @@
 package snowroller.myapplication.list;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,9 +28,22 @@ public class ListActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
+        //Set adapter for recyclerView
         adapter = new MyRecyclerViewAdapter(infoList);
         recyclerView.setAdapter(adapter);
 
+        //Add Swipe to delete functionality
+        SwipeToDeleteCallback swipeHandler = new SwipeToDeleteCallback(this) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                adapter.removeItem(viewHolder.getAdapterPosition());
+            }
+        };
+        ItemTouchHelper touchHelper = new ItemTouchHelper(swipeHandler);
+        touchHelper.attachToRecyclerView(recyclerView);
+
+
+        //Floating action button, register onclick listener
         findViewById(R.id.floatingActionButton).setOnClickListener(view -> {
             adapter.addItem(new ItemInfo(getResources().getDrawable(R.drawable.pumpa,getTheme()),"New Item","New Text"));
         });

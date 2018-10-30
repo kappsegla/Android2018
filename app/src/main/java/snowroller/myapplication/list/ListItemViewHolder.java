9a8eb0792987a4672e48f0ca00775c05942c1f55
Martin Blomberg <martin.blomberg@outlook.com>
2018-10-30 10:08:29 +0100

    ViewHolder

--- app/src/main/java/snowroller/myapplication/list/ListItemViewHolder.java
+++ app/src/main/java/snowroller/myapplication/list/ListItemViewHolder.java
@@ -3,9 +3,24 @@ package snowroller.myapplication.list;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import snowroller.myapplication.R;
 
 class ListItemViewHolder extends RecyclerView.ViewHolder {
+
+    public View itemView;
+    public ImageView imageView;
+    public TextView textTitle;
+    public TextView textSubTitle;
+
+
     public ListItemViewHolder(@NonNull View itemView) {
         super(itemView);
+        this.itemView = itemView;
+        imageView = itemView.findViewById(R.id.imageView);
+        textTitle = itemView.findViewById(R.id.textTitle);
+        textSubTitle = itemView.findViewById(R.id.textSubTitle);
     }
 }
